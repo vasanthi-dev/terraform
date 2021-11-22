7f26b588919587ec9a0544fea9bd7aa6f63c1772
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-22 15:19:13 -0500

    terraform basics

--- examples/main.tf
+++ examples/main.tf
@@ -25,6 +25,7 @@ resource "aws_security_group" "allow_sample" {
 
   egress = [
     {
+      description      = "egress"
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
