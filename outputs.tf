ad1a6cc3c8f580ed73f37b1fe19903198edd9205
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-21 23:01:20 -0500

    terraform basics

--- outputs.tf
+++ outputs.tf
@@ -13,3 +13,28 @@ output "xyz" {
 output "abc" {
   value = var.abc
 }
+
+variable "samplex" {
+  default = [
+    "Hello",
+    1000,
+    true,
+    "World"
+  ]
+}
+
+variable "sampley" {
+  default = {
+    dfg  = "Hello",
+    no  = 100,
+    boo = true
+  }
+}
+
+output "samplex" {
+  value = "value 1 = ${var.samplex[0]}, value 2 = ${var.samplex[1]}, value 3 = ${var.samplex[2]}, value 4 = ${var.samplex[3]}"
+}
+
+output "sampley" {
+  value = "value 1 = ${var.sampley["dfg"]}, value 2 = ${var.sampley["no"]}, value 3 = ${var.sampley["boo"]}"
+}
