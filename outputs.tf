a9208aabd30b234d1f95ba32efe44255956c19b0
vasanthi-dev <vasanthi2dev@gmail.com>
2021-11-21 22:40:17 -0500

    terraform basics

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,15 @@
+#output "abc" {
+#  value = "Hello World \nThis is Terraform"
+#}
+variable "xyz" {
+  default = "10"
+}
+variable "abc" {}
+
+output "xyz" {
+  value = var.xyz
+}
+
 output "abc" {
-  value = "Hello World \nThis is Terraform"
+  value = var.abc
 }
